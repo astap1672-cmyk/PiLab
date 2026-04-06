8ce53254c1adb78bae33707f651ce2d11f83cf9a
astap1672-cmyk <astap1672@gmail.com>
2026-04-06 11:27:49 +0200

    Add files via upload

---
+++ PiLab-main/Paint/zxcv.js
@@ -0,0 +1,50 @@
+let zoom = 1;
+const workspace = document.getElementById("workspace");
+const zoomValue = document.getElementById("zoomValue");
+const fileInput = document.getElementById("fileInput");
+const image = document.getElementById("image");
+const uploadBtn = document.getElementById("uploadBtn");
+
+function updateZoom() {
+    workspace.style.transform = `scale(${zoom})`;
+    zoomValue.textContent = Math.round(zoom * 100) + "%";
+}
+
+document.getElementById("zoomIn").addEventListener("click", () => {
+    zoom += 0.1;
+    updateZoom();
+});
+
+document.getElementById("zoomOut").addEventListener("click", () => {
+    if (zoom > 0.2) {
+        zoom -= 0.1;
+        updateZoom();
+    }
+});
+
+fileInput.addEventListener("change", function () {
+    const file = this.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        image.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+});
+
+document.getElementById("saveBtn").addEventListener("click", () => {
+    const link = document.createElement("a");
+    link.href = image.src;
+    link.download = "image.png";
+    link.click();
+});
+
+
+document.getElementById("undo").addEventListener("click", () => {
+    alert("Ошибка (я пидор и не доделал эту хуйню)");
+});
+
+document.getElementById("redo").addEventListener("click", () => {
+    alert("Ошибка (я пидор и не доделал эту хуйню)");
+});
